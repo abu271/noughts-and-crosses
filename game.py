6089c1d60d9be984a40b7956016af44794211182
abuDarda97 <abudarda166@gmail.com>
2020-06-26 18:11:42 +0100

    using classes

--- game.py
+++ game.py
@@ -1,46 +1,41 @@
 import utils
-import board
-import players
+from board import Board
 
 def start_game():
   print("Welcome to a game of Noughts and Crosses")
   play_game = input("Would like to play? ")
   play_game = utils.is_yes(play_game)
+  board = Board()
   
   while play_game:
-    players.assign_players()
-    game_over = False
+    player_o = input("Enter player O name: ")
+    player_x = input("Enter player X name: ")
+    board.set_player_names(player_x, player_o)
     player_x_turn = utils.true_or_false()
     player_x_won = False
     player_o_won = False
+    game_over = False
     board.reset()
     board.draw()
 
     while not game_over:
+
       if player_x_turn:
-        value = input("Your turn {} enter a number: ".format(players.player_x))
-        while not utils.valid_number(value):
-          value = input("{} please enter a number: ".format(players.player_x))
-        while not board.position_available(value):
-          value = input("{} that spot is taken try again: ".format(players.player_x))
+        value = board.player_input(player_x)
         board.update_board(value, "X")
         player_x_won = board.three_in_a_row("X")
         player_x_turn = False
       else:
-        value = input("Your turn {} enter a number: ".format(players.player_o))
-        while not utils.valid_number(value):
-          value = input("{} please enter a number: ".format(players.player_o))
-        while not board.position_available(value):
-          value = input("{} that spot is taken try again: ".format(players.player_o))
+        value = board.player_input(player_o)
         board.update_board(value, "O")
         player_o_won = board.three_in_a_row("O")
         player_x_turn = True
 
       if player_x_won:
-        print("THE WINNER IS {}".format(players.player_x))
+        print("THE WINNER IS {}".format(player_x))
         game_over = True
       elif player_o_won:
-        print("THE WINNER IS {}".format(players.player_o))
+        print("THE WINNER IS {}".format(player_o))
         game_over = True
       else: 
         is_draw = board.is_a_draw()
